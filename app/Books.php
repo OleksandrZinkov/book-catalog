11ddfa90322de88d6c796a59c0da156ce6bd4b55
Oleksandr Zinkovskyi <zenonrco@gmail.com>
2019-11-19 21:36:51 +0200

    заливаем ларавел на гитхаб

---
+++ app/Books.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Books extends Model
+{
+    protected $fillable=[
+
+        'title',
+        'author',
+        'annotations'
+
+
+
+    ];
+}
